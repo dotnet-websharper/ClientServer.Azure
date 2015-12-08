407278d60dd8ccdda6c2094cb417baf5c1903882
Loic Denuziere <loic.denuziere@intellifactory.com>
2015-12-08 11:00:23 +0100

    Update paket 2.34.0

--- build.cmd
+++ build.cmd
@@ -10,7 +10,8 @@ if not exist .paket (
   mkdir .paket
 )
 
-curl https://github.com/fsprojects/Paket/releases/download/1.2.0/paket.bootstrapper.exe -L --insecure -o .paket\paket.bootstrapper.exe
+curl https://github.com/fsprojects/Paket/releases/download/2.34.0/paket.bootstrapper.exe -L --insecure -o .paket\paket.bootstrapper.exe
+curl https://github.com/fsprojects/Paket/releases/download/2.34.0/paket.targets -L --insecure -o .paket\paket.targets
 
 if not exist .paket\paket.exe (
   .paket\paket.bootstrapper.exe
