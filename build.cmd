8133f8627ca5be21bc4e4c7daa926161d2f22a8b
David Emmerson <david.emmerson@gmail.com>
2016-01-14 21:38:53 +0000

    Fix quoting of msbuild env var for local builds. Allow build script to run if curl not present (as long as paket bootstrapper is). Use 14.0 or 12.0 msbuild.

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
 @ECHO OFF
-setlocal
+setlocal ENABLEDELAYEDEXPANSION
 
 REM Set your application folder relative to root
 set APPLICATION_FOLDER=MyApplication
@@ -10,7 +10,15 @@ if not exist .paket (
   mkdir .paket
 )
 
-curl https://github.com/fsprojects/Paket/releases/download/1.2.0/paket.bootstrapper.exe -L --insecure -o .paket\paket.bootstrapper.exe
+if not exist .paket\paket.bootstrapper.exe (
+    if not exist curl (
+        echo ====== Failed to find paket bootstrapper and curl was not available
+        exit 1
+    )
+        
+    curl https://github.com/fsprojects/Paket/releases/download/1.2.0/paket.bootstrapper.exe -L --insecure -o .paket\paket.bootstrapper.exe
+)
+
 
 if not exist .paket\paket.exe (
   .paket\paket.bootstrapper.exe
@@ -28,9 +36,23 @@ echo ====== Building... ======
 REM Azure provides MSBUILD_PATH and DEPLOYMENT_TARGET.
 REM If we're not deploying to Azure (eg. building locally),
 REM we need to set MSBUILD_PATH.
+
 if "%MSBUILD_PATH%" == "" (
-  set MSBUILD_PATH="%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe"
+  echo MSBUILD_PATH not found - assuming local build.
+  set "MSBUILD_PATH=%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe"
+
+  if not exist "!MSBUILD_PATH!" (
+      echo !MSBUILD_PATH! does not exist
+      set "MSBUILD_PATH=%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe"
+      echo Trying !MSBUILD_PATH!
+      if not exist "!MSBUILD_PATH!" (
+          echo ====== Failed to find msbuild ======
+          exit 1
+      )
+  )
 )
+
+echo using %MSBUILD_PATH%
 
 "%MSBUILD_PATH%" /p:Configuration=Release
 
